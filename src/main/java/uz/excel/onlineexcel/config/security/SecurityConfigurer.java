b73e56de837e48c3d85c3a656b05007651cdce17
jasur <jasurmutalov2001014@gmail.com>
2022-03-12 21:33:45 +0500

    commit secur

---
+++ src/main/java/uz/excel/onlineexcel/config/security/SecurityConfigurer.java
@@ -0,0 +1,62 @@
+package uz.excel.onlineexcel.config.security;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import uz.mutalov.travellog.config.security.filters.CustomAuthenticationFilter;
+import uz.mutalov.travellog.config.security.filters.CustomAuthorizationFilter;
+import uz.mutalov.travellog.service.auth.AuthUserService;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+    public final static String[] WHITE_LIST = {
+            "/api/login",
+            "/api/v1/refresh-token",
+            "/api/v1/auth/token",
+            "/swagger-ui/**",
+            "/api-docs/**",
+            "/api/v1/log/**"
+    };
+    private final AuthUserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests()
+                .antMatchers(WHITE_LIST)
+                .permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
+
