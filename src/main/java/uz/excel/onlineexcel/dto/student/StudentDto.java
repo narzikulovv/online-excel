8c8a3082d851a036479f14142500d6f5e05c4f78
macbookair <MuhammadkomilMurodillayev>
2022-03-14 17:54:09 +0500

    Student crud

--- src/main/java/uz/excel/onlineexcel/dto/student/StudentDto.java
+++ src/main/java/uz/excel/onlineexcel/dto/student/StudentDto.java
@@ -8,18 +8,33 @@ import uz.excel.onlineexcel.dto.base.GenericDto;
 @Builder
 @ToString
 public class StudentDto extends GenericDto {
+
     private String fullName;
+
     private String universityName;
+
     private String entranceYear; // o'qishgan kirgan yili
+
     private String graduationYear; // bitirgan yili
+
     private String faculty; // fakultet
+
     private String speciality; //yo'nalish
+
     private String studyType; //byudjet yoki kontrakt
+
     private String academicType; //kunduzgi, sirtqi yoki kechki
+
     private String diplomaSerial; //diplom seriyasi
+
     private String diplomaRegistrationNumber; // diplom reg raqami
+
     private String givenDate; // berilgan vaqti
+
     private String academicLevel; //magistr yoki bakalavr
+
     private String appendixNumber; // ilova raqami(nullable = false)
+
     private Long organizationId;
+
 }
